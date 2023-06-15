f71760cb9cda203c389c3927c8e0a667db345bac
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2023-06-15 19:47:27 -0400

    updated trial drop code

--- advanced-tutorials/trial-accounts/create-trial-drop.js
+++ advanced-tutorials/trial-accounts/create-trial-drop.js
@@ -1,4 +1,4 @@
-const { initKeypom, createTrialAccountDrop } = require('@keypom/core');
+const { initKeypom, createTrialAccountDrop, getEnv } = require('@keypom/core');
 const { UnencryptedFileSystemKeyStore } = require("@near-js/keystores-node");
 const { Account } = require('@near-js/accounts');
 const { Near } = require("@near-js/wallet-account");
@@ -10,6 +10,37 @@ const homedir = require("os").homedir();
 
 const funderAccountId = 'minqi.testnet';
 const NETWORK_ID = 'testnet';
+
+// What URLs should be returned
+const instances = {
+    guestBook: "http://localhost:1234/trial-url#ACCOUNT_ID/SECRET_KEY",
+    discovery: "http://localhost:3000/#trial-url/ACCOUNT_ID/SECRET_KEY"
+
+}
+// What contracts can the trial account call?
+const callableContracts = [
+    'guest-book.examples.keypom.testnet',
+    'v1.social08.testnet'
+]
+// What is the maximum amount of $NEAR that can be attached to a call for each callable contract?
+const maxAttachableNEARPerContract = [
+    '1',
+    '1'
+]
+// What methods can the trial account call?
+const callableMethods = [
+    ['*'],
+    ['*']
+]
+// How much $NEAR should be made available to the trial account when it's created?
+const startingBalanceNEAR = 0.5;
+// Once the trial account has spent this much $NEAR, the trial will be over.
+const trialEndFloorNEAR = 0.01;
+
+const numKeys = 1;
+const claimTrialAccounts = false;
+const includedCID = "bafkreidneri4ffymscahjprlapg4j62yleli73ncwdmopnkxpgczpusqn4"
+
 async function createTrialAccount() {
 	// Initiate connection to the NEAR blockchain.
     const CREDENTIALS_DIR = '.near-credentials';
@@ -35,56 +66,68 @@ async function createTrialAccount() {
 		network: NETWORK_ID
 	});
 
-	// What contracts can the trial account call?
-    const callableContracts = [
-        'guest-book.examples.keypom.testnet',
-        'v1.social08.testnet'
-    ]
-    // What is the maximum amount of $NEAR that can be attached to a call for each callable contract?
-    const maxAttachableNEARPerContract = [
-        '1',
-        '1'
-    ]
-	// What methods can the trial account call?
-	const callableMethods = [
-		['*'],
-        ['*']
-	]
-
     const wasmDirectory = `${require('path').resolve(__dirname, '..')}/trial-accounts/ext-wasm/trial-accounts.wasm`
-    const {keys} = await createTrialAccountDrop({
+    const {keys: {secretKeys}, dropId} = await createTrialAccountDrop({
 		account: fundingAccount,
-        numKeys: 1,
+        numKeys,
         contractBytes: [...readFileSync(wasmDirectory)],
-		// How much $NEAR should be made available to the trial account when it's created?
-        startingBalanceNEAR: 2.5,
+        startingBalanceNEAR,
         callableContracts,
         callableMethods,
         maxAttachableNEARPerContract,
-		// Once the trial account has spent this much $NEAR, the trial will be over.
-        trialEndFloorNEAR: 1.25
-    })  
+        trialEndFloorNEAR
+    })
 
-    
+    let accountIds = [];
+    if (claimTrialAccounts !== false) {
+        let incr = 0;
+        for (const secretKey of secretKeys) {
+            const newAccountId = `trial-drop-${dropId}_num-${incr}.testnet`
+            accountIds.push(newAccountId);
 
-    const guestBookInstance = "http://localhost:1234/trial-url#"
-    const keypomContractId = "v2.keypom.testnet"
-    const delimiter = "/"
-    const secretKey = keys.secretKeys[0]
+            await claimTrialAccountDrop({
+                secretKey,
+                desiredAccountId: newAccountId
+            })
+            incr += 1;
+        }
+    }
 
-    const alphaInstance = "http://localhost:3000/#trial-url/"
+    const {contractId: keypomContractId} = getEnv();
+    // Loop through the instances
+    for (const [key, value] of Object.entries(instances)) {
+        console.log(`\n\n`)
+        // insert the contractId and secret key into the base URL based on the CONTRACT_ID and SECRET_KEY field
+        const returnedURLs = [];
 
-    console.log(`
-    
-    Guest-Book App:
- 	${guestBookInstance}${keypomContractId}${delimiter}${secretKey}
-    
-    Alpha:
- 	${alphaInstance}${keypomContractId}${delimiter}${secretKey}
+        let incr = 0;
+        // loop through all secret keys
+        secretKeys.forEach((secretKey) => {
+            // insert the secret key into the base URL
+            let url = value.replace('SECRET_KEY', secretKey);
 
 
- 	Good Luck!
-    `)
+            if (claimTrialAccounts !== false) {
+                // insert the contract ID into the base URL
+                url = url.replace('ACCOUNT_ID', accountIds[incr]);
+            } else {
+                // insert the contract ID into the base URL
+                url = url.replace('ACCOUNT_ID', keypomContractId);
+            }
+
+            if (includedCID !== undefined) {
+                url = url + `?cid=${includedCID}`
+            }
+            // add the URL to the array of URLs
+            returnedURLs.push(url);
+            incr += 1;
+        });
+
+        console.log(`${key}:`)
+        console.log(returnedURLs.join('\n'));
+    }
+
+    console.log(`\n\n`)
 }
 
 createTrialAccount();
