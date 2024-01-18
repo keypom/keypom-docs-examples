6ebf04ecdfbfdf242f5b3d58613f9537f4948b3f
Min Qian Lu <59981815+minqianlu@users.noreply.github.com>
2024-01-18 10:51:07 -0500

    generate keys script

---
+++ random-scripts/generateKeys.js
@@ -0,0 +1,46 @@
+const { initKeypom, generateKeys } = require("@keypom/core"); 
+const { parseNearAmount } = require("@near-js/utils");
+const { UnencryptedFileSystemKeyStore } = require("@near-js/keystores-node");
+const { Near } = require("@near-js/wallet-account");
+const { Account } = require("@near-js/accounts");
+const path = require("path");
+const homedir = require("os").homedir();
+
+async function pubkey(){
+	// Initiate connection to the NEAR blockchain.
+	const network = "testnet"
+	const CREDENTIALS_DIR = ".near-credentials";
+	const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
+	const YOUR_ACCOUNT = "minqi.testnet";
+	
+	let keyStore = new UnencryptedFileSystemKeyStore(credentialsPath);
+
+	let nearConfig = {
+	    networkId: network,
+	    keyStore: keyStore,
+	    nodeUrl: `https://rpc.${network}.near.org`,
+	    walletUrl: `https://wallet.${network}.near.org`,
+	    helperUrl: `https://helper.${network}.near.org`,
+	    explorerUrl: `https://explorer.${network}.near.org`,
+	};
+
+	let near = new Near(nearConfig);
+	const fundingAccount = new Account(near.connection, YOUR_ACCOUNT);
+
+	// If a NEAR connection is not passed in and is not already running, initKeypom will create a new connection
+	// Here we are connecting to the testnet network
+	await initKeypom({
+	    near,
+	    network
+	});
+
+    let numKeys = 1;
+
+    let keys = await generateKeys({
+        numKeys
+    });
+
+    console.log(keys)
+
+}
+pubkey()
